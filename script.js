7d0bdb3f66ab478018ab59c572682458c13fbce9
raiza <raizaksilva@gmail.com>
2026-03-12 22:55:15 -0300

    feat: ajuste na estilização e adiciona scroll animado

--- script.js
+++ script.js
@@ -19,3 +19,17 @@ function enviarWhats(event) {
     window.open(url, '_blank')
 
 }
+
+const elements = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach(entry=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add("active");
+    }
+  });
+},{
+  threshold:0.2
+});
+
+elements.forEach(el => observer.observe(el));
